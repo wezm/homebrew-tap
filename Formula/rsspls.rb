8bee4d42fcf0b9455a37a84823cf65dd34349def
Wesley Moore <wes@wezm.net>
2022-10-14 20:02:44 +1000

    rsspls: 0.4.1

--- Formula/rsspls.rb
+++ Formula/rsspls.rb
@@ -4,25 +4,23 @@
 class Rsspls < Formula
   desc "Generate RSS feeds from web pages"
   homepage "https://github.com/wezm/rsspls"
-  version "0.3.0"
+  version "0.4.1"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://releases.wezm.net/rsspls/#{version}/rsspls-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "13c8e682691c4b288b3209a17549c418805d6b71895b14299f9cd9298e1f068a"
+    if Hardware::CPU.intel? || Hardware::CPU.arm?
+      url "https://releases.wezm.net/rsspls/#{version}/rsspls-#{version}-universal-apple-darwin.tar.gz"
+      sha256 "3898ac570855b932217eddb7b6a50aa2eac31138043db0a92a9d5f1467568bca"
 
       def install
         bin.install "rsspls"
       end
-    # elsif Hardware::CPU.arm?
-    #   fail "sorry there's no arm mac build yet. See issue #9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://releases.wezm.net/rsspls/#{version}/rsspls-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "0740d51dacea7d009b716896f2fb89592ae48763c0381537f0220f7bc156fa63"
+      sha256 "20e7fce366f72f02b12f380a119814a6e208684d855f5c32868528b61f31137e"
 
       def install
         bin.install "rsspls"
